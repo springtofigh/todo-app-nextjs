513c98f71ecc75bc16cda8ae710f9b011992d255
BT <bahartofigh@gmail.com>
2023-07-07 23:51:05 +0430

    add margin button

--- containers/Layout/index.js
+++ containers/Layout/index.js
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="bg-zinc-900 min-h-screen">
+    <div className="bg-zinc-900 min-h-screen mb-6">
       <Header />
       <div className="container p-2 xl:max-w-screen-xl mx-auto">{children}</div>
       <Footer />
