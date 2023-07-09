dd923396d11f6af475342b558eb61d18fb8d766e
BT <bahartofigh@gmail.com>
2023-07-10 00:54:41 +0430

    create ProfileTodo component and add to todos page.

---
+++ components/todos/ProfileTodo.js
@@ -0,0 +1,34 @@
+import { CheckIcon , TrashIcon } from '@heroicons/react/outline'
+import Link from 'next/link'
+
+const ProfileTodo = ({ data, onDelete, onComplete }) => {
+    return (
+        <div className="w-full p-2 md:p-4 rounded-xl">
+                {data.map((todo) => {
+                return(
+                <div key={todo._id} className="w-full bg-white font-medium p-2 mb-2 md:p-4 rounded-xl">
+                    <div className="flex items-center justify-between border border-gray-100 mb-4 p-3 md:p-4 rounded-xl">
+                    <Link href={`/todos/${todo._id}`}>
+                    <a>
+                    <span className={`${todo.isCompleted ? "line-through" : ""}`}>{todo.title}</span>
+                    </a>
+                    </Link>
+                    <div className="flex gap-x-3 items-center">
+                    <button className="" onClick={() => onComplete(todo._id)}>
+                    {todo.isCompleted ? (
+                    <CheckIcon className="w-6 h-6 stroke-green-400" />
+                    ) : <span className='w-5 h-5 block border-2 border-gray-600 rounded-full'></span>}
+                    </button>
+                    <button onClick={() => onDelete(todo._id)} >
+                        <TrashIcon className="w-6 h-6 stroke-red-400" />
+                    </button>
+                    </div>
+                    </div>
+                </div>
+            );
+                })}
+        </div>
+    );
+    };
+
+export default ProfileTodo
--- pages/todos.js
+++ pages/todos.js
@@ -1,18 +1,43 @@
-import Layout from "../containers/Layout"
+import axios from "axios";
+import { useState } from "react"
 import { useSession , getSession } from "next-auth/react"
+import dbConnect from "../server/utils/dbConnect"
+import Todo from "../server/models/todo"
+import Layout from "../containers/Layout"
+import ProfileTodo from "../components/todos/ProfileTodo"
 
 
-
-const todos = () => {
+const todos = ({ todos }) => {
     const { data: session , status } = useSession();
     console.log(session);
+    const [data, setData] = useState(todos);
+
+    const deleteTodo = (id) => {
+      axios
+      .delete(`/api/todos/${id}`)
+      .then(({ data }) => {
+        setData(data.todos);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err))
+    };
+
+    const completeHandler = (id) => {
+      console.log(id);
+      axios
+      .put(`/api/todos/complete/${id}`)
+      .then(({data}) => {
+        setData(data.todos);
+      })
+      .catch((err) => console.log(err))
+    }
+
   return (
     <Layout>
-        <h1 className="text-slate-100 text-center">
-        {session.user.name},
-          سلام
-        خوش اومدی
+        <h1 className="text-slate-100 text-right">
+        در زیر قابل مشاهده است {session.user.name} لیست تودو های
         </h1>
+        <ProfileTodo data={data} onDelete={deleteTodo} onComplete={completeHandler} />
     </Layout>
   )
 }
@@ -32,8 +57,12 @@ export async function getServerSideProps(ctx) {
     };
   }
 
+  dbConnect();
+  const todos = await Todo.find({})
+
   return {
     props: {
+      todos: JSON.parse(JSON.stringify(todos)),
       session,
     },
   };
