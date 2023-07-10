2360e128300979188df7dcd7fd7928aea496abb8
BT <bahartofigh@gmail.com>
2023-07-11 02:20:42 +0430

    change title of project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,19 @@
 import '../../styles/globals.css'
 import { SessionProvider } from "next-auth/react"
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps: { session , ...pageProps } }) {
 
   return (
+    <>
+    <Head>
+        <title>Todo List</title>
+    </Head>
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
+    </>
+
   )
 }
 
