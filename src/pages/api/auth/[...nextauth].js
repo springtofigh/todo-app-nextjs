f230c871fece418f217a33540e11d5882e51600a
BT <bahartofigh@gmail.com>
2023-08-13 21:32:13 +0430

    test NextAuth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,24 +1,60 @@
+// import NextAuth from "next-auth";
+// import GithubProvider from "next-auth/providers/github";
+// import { MongoDBAdapter } from "@auth/mongodb-adapter"
+// import clientPromise from "@/server/lib/mongodb"
+
+// export default NextAuth({
+//   Configure one or more authentication providers
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//     }),
+//     ...add more providers here
+//   ],
+//   session: {
+//     strategy: "jwt"
+//   },
+//   jwt: {
+//     secret: process.env.SECRET_JWT,
+//   },
+//   secret: process.env.NEXTAUTH_URL,
+//   adapter: MongoDBAdapter(clientPromise),
+//   callbacks: {
+//     async session({ session, token }) {
+//       session.user.id = token.id;
+//       session.accessToken = token.accessToken;
+//       return session;
+//     },
+//     async jwt({ token, user, account }) {
+//       if (user) {
+//         token.id = user.id;
+//       }
+//       if (account) {
+//         token.accessToken = account.access_token;
+//       }
+//       return token;
+//     },
+//   },
+
+// });
+
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import clientPromise from "@/server/lib/mongodb"
+import GitHubProvider from 'next-auth/providers/github'
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from "@/lib/mongodb"
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
-    GithubProvider({
+    GitHubProvider({
       clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    // ...add more providers here
+      clientSecret: process.env.GITHUB_SECRET
+    })
   ],
   session: {
-    strategy: "jwt"
+    strategy: 'jwt'
   },
-  jwt: {
-    secret: process.env.SECRET_JWT,
-  },
-  secret: process.env.NEXTAUTH_URL,
+  secret: process.env.NEXTAUTH_SECRET,
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
     async session({ session, token }) {
