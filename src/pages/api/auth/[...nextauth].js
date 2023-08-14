6e452e7d121f3c3b31202e1f5ebdd51886fd0ce4
BT <bahartofigh@gmail.com>
2023-08-14 18:34:35 +0430

    change MongoDBAdapter import

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -41,7 +41,7 @@
 
 import NextAuth from "next-auth";
 import GitHubProvider from 'next-auth/providers/github'
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from "@/server/lib/mongodb"
 
 export default NextAuth({
